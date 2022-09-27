2d7fb81bcdc665bef7cdf64f7e7a5bb5a605712e
crissferro <crissferro@gmail.com>
2022-09-27 10:06:01 -0300

    cambio react

--- clase06-react/src/App.js
+++ clase06-react/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1 class="card-title">Clase 06</h1>
+      <h1 class="card-title">Clase 06 - React</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
